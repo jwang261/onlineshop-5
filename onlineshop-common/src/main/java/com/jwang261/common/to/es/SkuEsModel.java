08fafa24f6bca1b4c691983fa0103d1ff0db2bb2
jwang261 <jwang261@syr.edu>
2020-08-31 19:35:31 -0400

    08/29/2020 2/3 es Start

--- onlineshop-common/src/main/java/com/jwang261/common/to/es/SkuEsModel.java
+++ onlineshop-common/src/main/java/com/jwang261/common/to/es/SkuEsModel.java
@@ -1,8 +1,12 @@
-package com.jwang261.common.to;
+package com.jwang261.common.to.es;
+
+import lombok.Data;
 
 /**
  * @author jwang261
  * @date 2020/8/31 7:33 PM
  */
+@Data
 public class SkuEsModel {
+
 }
