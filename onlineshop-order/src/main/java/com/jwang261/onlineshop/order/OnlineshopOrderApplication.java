4e57ab683459550df3924e7ca46915c52165b7b7
jwang261 <jwang261@syr.edu>
2021-01-13 23:53:59 -0500

    1/13/2021 262. 65.5% finished

--- onlineshop-order/src/main/java/com/jwang261/onlineshop/order/OnlineshopOrderApplication.java
+++ onlineshop-order/src/main/java/com/jwang261/onlineshop/order/OnlineshopOrderApplication.java
@@ -1,6 +1,7 @@
 package com.jwang261.onlineshop.order;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,6 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableRabbit
 @MapperScan("com.jwang261.onlineshop.order.dao")
 public class OnlineshopOrderApplication {
     public static void main(String[] args) {
